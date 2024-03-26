09d5f64767f96e390f4d3336085c8f8992cb4f95
kazyalich <162959456+kazyalich@users.noreply.github.com>
2024-03-27 01:33:03 +0600

    Add files via upload

---
+++ alik7.php
@@ -0,0 +1,9 @@
+<?php
+
+function sum($num1, $num2) {
+    return $num1 + $num2;
+}
+
+// Пример использования функции
+$result = sum(5, 3);
+echo "Сумма чисел: " . $result; // Выведет: Сумма чисел: 8
